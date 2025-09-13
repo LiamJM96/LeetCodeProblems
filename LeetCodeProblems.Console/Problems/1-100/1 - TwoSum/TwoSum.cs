d2ee2fac53a0694a36aa84386cfa07c4cd7cfb53
Liam Mcilwee <liammcilwee@outlook.com>
2025-09-13 19:49:23 +0100

    Improved xml documentation

--- LeetCodeProblems.Console/Problems/1-100/1 - TwoSum/TwoSum.cs
+++ LeetCodeProblems.Console/Problems/1-100/1 - TwoSum/TwoSum.cs
@@ -3,12 +3,43 @@ using LeetCodeProblems.Console.Core;
 namespace LeetCodeProblems.Console.Problems;
 
 /// <summary>
-/// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-///
-/// You may assume that each input would have exactly one solution, and you may not use the same element twice.
-///
-/// You can return the answer in any order.
+/// Return indices of two numbers in nums that add up to target.
 /// </summary>
+/// <remarks>
+/// <para>
+/// You may assume that each input has exactly one solution, and you may not use the same element twice.
+/// The answer can be returned in any order.
+/// </para>
+/// </remarks>
+/// <example>
+/// <code language="text">
+/// Input:
+///   nums = [2,7,11,15]
+///   target = 9
+/// Output:
+///   [0,1]
+/// Explanation:
+///   nums[0] + nums[1] == 9
+/// </code>
+/// </example>
+/// <example>
+/// <code language="text">
+/// Input:
+///   nums = [3,2,4]
+///   target = 6
+/// Output:
+///   [1,2]
+/// </code>
+/// </example>
+/// <example>
+/// <code language="text">
+/// Input:
+///   nums = [3,3]
+///   target = 6
+/// Output:
+///   [0,1]
+/// </code>
+/// </example>
 
 [LeetCode(1, "Two Sum", Difficulty.Easy, "https://leetcode.com/problems/two-sum/")]
 public class TwoSum : ProblemBase
